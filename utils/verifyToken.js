215b6c07af63b6090cca91d259b1257863f474fb
Cheastear <56431973+Cheastear@users.noreply.github.com>
2024-08-15 23:10:59 +0300

    verify token with database

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,14 +1,26 @@
 import passport from "passport";
+import { getById } from "../services/usersServices.js";
+import ApiError from "./ApiError.js";
 
 const verifyToken = async (req, res, next) => {
-  passport.authenticate("jwt", { session: false }, (err, user) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
     if (!user || err) {
       return res.status(401).json({
         message: "Unauthorized",
       });
     }
-    req.user = user;
-    next();
+
+    try {
+      const userDb = await getById(user.id);
+      if (req.headers.authorization.replace("Bearer ", "") !== userDb.token) {
+        next(new ApiError(401, "Unauthorized"));
+        return;
+      }
+      req.user = user;
+      next();
+    } catch (err) {
+      res.status(500, "Server error");
+    }
   })(req, res, next);
 };
 
